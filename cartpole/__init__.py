ca7ffe814e36c478b03b6e143f5b2eadac71798b
inkyusa <enddl22@gmail.com>
2019-08-08 17:51:32 +0200

    folder re-structuring, putting everything in one folder

--- cartpole/__init__.py
+++ cartpole/__init__.py
@@ -1,4 +1,4 @@
 import os
 from _raisim_gym import *
 
-__CARTPOLE_RESOURCE_DIRECTORY__ = os.path.dirname(os.path.realpath(__file__))+'/cartpole'
+__CARTPOLE_RESOURCE_DIRECTORY__ = os.path.dirname(os.path.realpath(__file__))+'/rsc'
