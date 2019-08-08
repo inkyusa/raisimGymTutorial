50651b51acabab665a66ecb484990ceacf677694
inkyusa <enddl22@gmail.com>
2019-08-08 17:32:36 +0200

    adjust environment.hpp searching path

--- scripts/cartpole_tutorial.py
+++ scripts/cartpole_tutorial.py
@@ -13,7 +13,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--cfg', type=str, default=os.path.abspath(__RSCDIR__ + "/default_cfg.yaml"),
                     help='configuration file')
-parser.add_argument('-m', '--mode', required=True, help='set mode either train or test', type=str, default='train')
+parser.add_argument('-m', '--mode', help='set mode either train or test', type=str, default='train')
 parser.add_argument('-w', '--weight', help='trained weight path', type=str, default='')
 args = parser.parse_args()
 mode = args.mode
@@ -21,10 +21,10 @@ cfg_abs_path = parser.parse_args().cfg
 cfg = YAML().load(open(cfg_abs_path, 'r'))
 
 # save the configuration and other files
-rsg_root = os.path.dirname(os.path.abspath(__file__)) + '/../'
+rsg_root = os.path.dirname(os.path.abspath(__file__)) + '/../cartpole'
 log_dir = rsg_root + '/data'
 saver = ConfigurationSaver(log_dir=log_dir + '/Cartpole_tutorial',
-                           save_items=[rsg_root + 'raisim_gym/env/env/cartpole/Environment.hpp', cfg_abs_path])
+                           save_items=[rsg_root + '/Environment.hpp', cfg_abs_path])
 
 # create environment from the configuration file
 if args.mode == "test": # for test mode, force # of env to 1
